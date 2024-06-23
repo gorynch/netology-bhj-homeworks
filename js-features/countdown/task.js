6e2bdf193b686d39050c4e2598a5bc8269ce07f5
gorynch <i231081@yandex.ru>
2024-06-23 10:22:05 +0300

    Update task.js in countdown

--- js-features/countdown/task.js
+++ js-features/countdown/task.js
@@ -1,3 +1,12 @@
-alert('Lets start');
-const startTimer = document.getElementById('timer');
-console.log(' start from : ', startTimer);
+let count = Number(document.getElementById('timer').textContent);
+const timerElement = document.getElementById('timer');
+
+let interval = setInterval( function() {
+    if (count == 0 ) {
+        clearInterval(interval);
+        alert('Вы победили в конкурсе!');
+        return;
+    }
+    count = count - 1;
+    timerElement.textContent = count;
+}, 1000);
