2fa34ab2414db23ef36ab561b44d058367b47492
gorynch <i231081@yandex.ru>
2024-07-27 12:50:50 +0300

    Changed task.js

--- document-structure/tooltip/task.js
+++ document-structure/tooltip/task.js
@@ -1,60 +1,67 @@
-const tooltipActivateClass = 'tooltip_active';
-const link = document.querySelectorAll('.has-tooltip');
-const tooltip = document.querySelector('.tooltip');
-const gap4tip = 5;
 const hideTipOnSceoll = true;
+const gap4tip = 5;
 const position = 'bottom'
 // const position = 'top'
 // const position = 'left'
 // const position = 'right'
 
 function showTooltip(el) {
-    let rectHasTip = el.currentTarget.getBoundingClientRect();
-    tooltip.textContent = el.currentTarget.title;
-    tooltip.setAttribute('data-position', el.currentTarget.pos);
-    tooltip.classList.add(tooltipActivateClass);
-    let rectTip = tooltip.getBoundingClientRect();
-    switch (tooltip.dataset.position) {
-        case 'bottom' : if (window.innerHeight - rectHasTip.top - rectHasTip.height - rectTip.height - gap4tip >= 0) {
-                            tooltip.style.left = rectHasTip.left + 'px';
-                            tooltip.style.top = rectHasTip.top + Number(rectHasTip.height.toFixed()) + gap4tip + 'px';
-                        }
-                        else {
-                            tooltip.classList.remove(tooltipActivateClass);
-                            console.log('tip out of visible area')
-                        }
-                        break;
-        case 'top' :    if (rectHasTip.top - rectHasTip.height - rectTip.height - gap4tip >= 0) {
-                            tooltip.style.left = rectHasTip.left + 'px';
-                            tooltip.style.top = rectHasTip.top - Number(rectTip.height.toFixed()) - gap4tip + 'px';
-                        }
-                        else {
-                            tooltip.classList.remove(tooltipActivateClass);
-                            console.log('tip out of visible area')
-                        }
-                        break;
-        case 'left' :       if (rectHasTip.left - rectTip.width - gap4tip >= 0) {
-                                tooltip.style.left = rectHasTip.left - rectTip.width - gap4tip + 'px';
-                                tooltip.style.top = rectHasTip.top + 'px';
+    console.log('show tip');
+    console.log('textContent', document.querySelector('.tooltip').textContent.trim(),'current',el.currentTarget.title)
+    if ((document.querySelector('.tooltip').textContent !== el.currentTarget.title) || !(document.querySelector('.tooltip').classList.contains('tooltip_active'))) {
+        let rectHasTip = el.currentTarget.getBoundingClientRect();
+        document.querySelector('.tooltip').textContent = el.currentTarget.title;
+        document.querySelector('.tooltip').setAttribute('data-position', el.currentTarget.pos);
+        document.querySelector('.tooltip').classList.add('tooltip_active');
+        let rectTip = document.querySelector('.tooltip').getBoundingClientRect();
+        console.log('position',document.querySelector('.tooltip').dataset.position);
+        switch (document.querySelector('.tooltip').dataset.position) {
+            case 'bottom' : if (window.innerHeight - rectHasTip.top - rectHasTip.height - rectTip.height - gap4tip >= 0) {
+                                document.querySelector('.tooltip').style.left = rectHasTip.left + 'px';
+                                document.querySelector('.tooltip').style.top = rectHasTip.top + Number(rectHasTip.height.toFixed()) + gap4tip + 'px';
+                                console.log('top:',document.querySelector('.tooltip'));
+                                console.log('top:',document.querySelector('.tooltip').style.top);
                             }
                             else {
-                                tooltip.classList.remove(tooltipActivateClass);
+                                document.querySelector('.tooltip').classList.remove('tooltip_active');
                                 console.log('tip out of visible area')
                             }
                             break;
-        case 'right' :      if (window.innerHeight - rect.top - rect.height - rectTip.height - gap4tip >= 0) {
-                                tooltip.style.left = rect.left + 'px';
-                                tooltip.style.top = rect.top + Number(rect.height.toFixed()) + gap4tip + 'px';
+            case 'top' :    if (rectHasTip.top - rectHasTip.height - rectTip.height - gap4tip >= 0) {
+                                document.querySelector('.tooltip').style.left = rectHasTip.left + 'px';
+                                document.querySelector('.tooltip').style.top = rectHasTip.top - Number(rectTip.height.toFixed()) - gap4tip + 'px';
                             }
                             else {
-                                tooltip.classList.remove(tooltipActivateClass);
+                                document.querySelector('.tooltip').classList.remove('tooltip_active');
                                 console.log('tip out of visible area')
                             }
+                            break;
+            case 'left' :       if (rectHasTip.left - rectTip.width - gap4tip >= 0) {
+                                    document.querySelector('.tooltip').style.left = rectHasTip.left - rectTip.width - gap4tip + 'px';
+                                    document.querySelector('.tooltip').style.top = rectHasTip.top + 'px';
+                                }
+                                else {
+                                    document.querySelector('.tooltip').classList.remove('tooltip_active');
+                                    console.log('tip out of visible area')
+                                }
+                                break;
+            case 'right' :      if (window.innerHeight - rect.top - rect.height - rectTip.height - gap4tip >= 0) {
+                                    document.querySelector('.tooltip').style.left = rect.left + 'px';
+                                    document.querySelector('.tooltip').style.top = rect.top + Number(rect.height.toFixed()) + gap4tip + 'px';
+                                }
+                                else {
+                                    document.querySelector('.tooltip').classList.remove('tooltip_active');
+                                    console.log('tip out of visible area')
+                                }
+                            }
+        document.querySelector('.tooltip').style.zIndex = '99'
     }
-    tooltip.style.zIndex = '99'
+    else (
+        document.querySelector('.tooltip').classList.toggle('tooltip_active')
+    )
 };
 
-link.forEach((el) => {
+document.querySelectorAll('.has-tooltip').forEach((el) => {
     el.addEventListener('click', showTooltip, false)
     el.title = el.title;
     el.pos = position;
@@ -62,6 +69,6 @@ link.forEach((el) => {
 
 window.addEventListener("scroll", () => {
     if (hideTipOnSceoll) {
-        tooltip.classList.remove(tooltipActivateClass);
+        document.querySelector('.tooltip').classList.remove('tooltip_active');
     }
 });
