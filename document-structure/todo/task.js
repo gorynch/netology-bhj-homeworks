e1e08768c03cd59f8378747864832e1541213f2e
gorynch <i231081@yandex.ru>
2024-07-27 12:49:58 +0300

    Changed task.js

--- document-structure/todo/task.js
+++ document-structure/todo/task.js
@@ -1,52 +1,29 @@
-const form = document.querySelector('form');
-const taskText = document.getElementById('task__input');
-const tasksList = document.getElementById('tasks__list');
-const taskTemplate =    "<div class='task'>\
-                            <div class='task__title'>\
-                                Test\
-                            </div>\
-                            <a href='#' class='task__remove'>\
-                                &times;\
-                            </a>\
-                        </div>";
-const taskDiv = document.querySelector('.task__title');
-const taskId = 'task';
-const taskClass = '.task';
-let taskCount = 0;
 
 function removeTask (el) {
-    document.getElementById(taskId+el.currentTarget.id).remove();
+    el.currentTarget.closest('.task').remove();
 }
 
 function addTask() {
-    let taskDiv0 = document.querySelector(taskClass);
-    if (taskDiv0) {
-        if (taskText.value) {
-            let newTsak = taskDiv0.cloneNode(true);
-            newTsak.setAttribute('id', taskId+taskCount);
-            tasksList.appendChild(newTsak);
-        }
-        else {
-            console.log('enter some text');
-        }
+    event.preventDefault();
+    if (document.getElementById('task__input').value) {
+        let task = document.createElement('div')
+        task.classList.add('task');
+        let taskTitle = document.createElement('div');
+        taskTitle.classList.add('task__title');
+        taskTitle.textContent = document.getElementById('task__input').value;
+        task.appendChild(taskTitle);
+        let taskRemove = document.createElement('a');
+        taskRemove.setAttribute('href','#');
+        taskRemove.innerHTML = '&times;';
+        taskRemove.classList.add('task__remove');
+        taskRemove.addEventListener('click', removeTask, false)
+        task.appendChild(taskRemove)
+        document.getElementById('tasks__list').appendChild(task)
     }
     else {
-        if (taskText.value) {
-            tasksList.innerHTML = taskTemplate;
-            document.querySelector(taskClass).setAttribute('id', taskId+taskCount);
-        }
-        else {
-            console.log('enter some text');
-        }
+        console.log('enter some text');
     }
-    if (document.getElementById(taskId+taskCount)) {
-        document.getElementById(taskId+taskCount).querySelector('.task__title').textContent = taskText.value.trim();
-        let el = document.getElementById(taskId+taskCount).children[1];
-        el.addEventListener('click', removeTask, false)
-        el.id = taskCount;
-        taskCount++;
-    }
-    form.reset();
+    document.querySelector('form').reset();
 }
 
-form.addEventListener('submit', addTask, false);
+document.querySelector('form').addEventListener('submit', addTask, false);
