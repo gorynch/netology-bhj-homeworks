4b039c6a3185660d62b73cf315b0b69cce4789fe
gorynch <i231081@yandex.ru>
2024-07-30 22:51:57 +0300

    Initial commit

---
+++ client-state/text-editor/task.js
@@ -0,0 +1,13 @@
+if (localStorage.getItem('textArea') != '') {
+    document.getElementById('editor').value = localStorage.getItem('textArea');
+}
+
+document.getElementById('editor').addEventListener('input', () => {
+    localStorage.setItem('textArea', document.getElementById('editor').value);
+})
+
+
+document.getElementById('btnClear').addEventListener('click', () => {
+    document.getElementById('editor').value = '';
+    localStorage.removeItem('textArea');
+})
