e425dc6a5ffcda6e4d9998d49682bf9dcf2c9eb6
gorynch <i231081@yandex.ru>
2024-08-01 11:08:59 +0300

    optimise code

--- client-state/auth/task.js
+++ client-state/auth/task.js
@@ -1,32 +1,44 @@
 const authUrl = 'https://students.netoservices.ru/nestjs-backend/auth';
 
-function authCheck (url, name, pwd) {
-    const xhttp = new XMLHttpRequest;
-    xhttp.open('POST', url, true);
-    xhttp.setRequestHeader('Content-type', 'application/x-www-form-urlencoded');
-    xhttp.onreadystatechange = function() {
-        if (this.readyState == 4 && (this.status == 200 || this.status == 201)) {
-            if (JSON.parse(this.response).success) {
-                document.getElementById('user_id').textContent = JSON.parse(this.response).user_id;
-                document.getElementById('welcome').classList.add('welcome_active');
-            }
-            else {
-                document.getElementById('welcome').classList.remove('welcome_active');
-                alert('Неверный логин/пароль');
-            }
-        }
-    };
-    xhttp.send(`login=${name}&password=${pwd}`);
+if (localStorage.getItem('user_id')) {
+    document.getElementById('user_id').textContent = localStorage.getItem('user_id');
+    document.getElementById('welcome').classList.add('welcome_active');
 }
+
+function authCheck (url, name, pwd) {
+    if (!localStorage.getItem('user_id')) {
+        const formData = new FormData();
+        formData.append('login', name);
+        formData.append('password', pwd);
+        const xhttp = new XMLHttpRequest;
+        xhttp.open('POST', url, true);
+        xhttp.responseType = 'json'
+        xhttp.onload = function () {
+                if (this.response.success) {
+                    document.getElementById('user_id').textContent = this.response.user_id;
+                    localStorage.setItem('user_name', name);
+                    localStorage.setItem('user_id', this.response.user_id);
+                    document.getElementById('welcome').classList.add('welcome_active');
+                }
+                else {
+                    document.getElementById('welcome').classList.remove('welcome_active');
+                    alert('Неверный логин/пароль');
+                }
+            }
+        xhttp.send(formData);
+        }
+};
 
 document.getElementById('signin__btn').addEventListener('click', () => {
     event.preventDefault();
     authCheck(authUrl, document.getElementsByName('login')[0].value, document.getElementsByName('password')[0].value);
-    document.getElementsByName('login')[0].value = '';
-    document.getElementsByName('password')[0].value = '';
+    document.getElementById('signin__form').reset();
 });
 
 document.getElementById('signout__btn').addEventListener('click', () => {
     event.preventDefault();
+    localStorage.removeItem('user_id');
+    document.getElementById('user_id').textContent = '';
+    document.getElementById('welcome').classList.remove('welcome_active');
     alert('Sing out')
 });
